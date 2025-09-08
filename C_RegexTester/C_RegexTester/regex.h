26eb88a0efb581736f3ff309346fe7dbba949ce1
ssuchy00 <patrykplaypl8@gmail.com>
2025-09-08 20:39:57 +0200

    [REGEX] changed constructor and destructor name
    FIX

---
+++ C_RegexTester/C_RegexTester/regex.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdlib.h>
+
+typedef void* regex_loop;
+
+typedef struct regex_t {
+	regex_loop* loops;
+	size_t elements_count;
+} regex;
+
+void regex_create(regex* out);
+void regex_delete(regex* it);
